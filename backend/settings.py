eae898b8e86b38fee611cbf6ccde68d6ef596d0b
szczureslaw <amog7776@gmail.com>
2025-10-29 14:26:17 +0100

    updated backend

--- backend/settings.py
+++ backend/settings.py
@@ -130,7 +130,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -140,5 +141,3 @@ AUTH_USER_MODEL = 'users.User'
 
 
 
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
