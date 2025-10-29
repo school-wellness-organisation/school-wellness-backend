5b9ce3d13756153fdce2e2329a42e0bff8c7263a
szczureslaw <amog7776@gmail.com>
2025-10-29 14:23:21 +0100

    updated backend

--- backend/settings.py
+++ backend/settings.py
@@ -138,3 +138,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'users.User'
 
+
+
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
